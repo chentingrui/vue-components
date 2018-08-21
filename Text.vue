ce4349e3c7ad0573312d69a128b58825f6d56f95
chentingrui <30311805+chentingrui@users.noreply.github.com>
2018-08-21 18:25:11 +0800

    Add files via upload

---
+++ Text.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hr-text" :class="type">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String
+  },
+  data () {
+    return {}
+  },
+  created: function () {
+  }
+}
+</script>
+<style scoped>
+.hr-text.layer-title{
+  font-family: Ubuntu-Bold;
+  color: rgb(51, 51, 51);
+  font-size: 24px;
+  padding: 50px 0 0 0;
+  text-align: center;
+}
+
+.hr-text.layer-item-content{
+  font-family: Ubuntu-Medium;
+  color: rgb(51, 51, 51);
+  font-size: 18px;
+}
+
+.hr-text.layer-item-title{
+  font-family: Ubuntu-Medium;
+  color: rgb(51, 51, 51);
+  font-size: 14px;
+}
+
+.hr-text.category-label{
+  font-family: Ubuntu-Bold;
+  color: rgb(51, 51, 51);
+  font-size: 22px;
+}
+
+.hr-text.header-label{
+  color: rgb(51, 51, 51);
+  font-size: 17px;
+  font-family: Ubuntu-Medium;
+}
+
+.hr-text.main-content{
+  font-family: Ubuntu-Regular;
+  color: rgb(51, 51, 51);
+  font-size: 15px;
+}
+
+.hr-text.date-stamp{
+  font-family: Ubuntu-Regular;
+  color: rgb(170, 180, 185);
+  font-size: 14px;
+}
+</style>
