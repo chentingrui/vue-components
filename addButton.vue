f4de2203628b73e5fd34eac30c30d0f75e4b702e
chentingrui <30311805+chentingrui@users.noreply.github.com>
2018-08-21 18:18:40 +0800

    Add files via upload

---
+++ addButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="hr-add-btn" @click="onClick">
+    <i class="fa fa-plus"></i>
+    <span>{{text}}</span>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+<style scoped>
+.hr-add-btn{
+  color: #5ba54b;
+  font-size: 14px;
+  line-height: 20px;
+  cursor: pointer;
+  background-size: 16px;
+}
+.hr-add-btn i{
+  padding-right: 4px;
+}
+</style>
